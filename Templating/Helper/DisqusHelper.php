e5d5a23f09b1c6274cca5d890c582fd05d6ff5a5
Alex Demchenko <pilot@pesochin.net>
2012-12-20 17:52:21 +0200

    Update error message

--- Templating/Helper/DisqusHelper.php
+++ Templating/Helper/DisqusHelper.php
@@ -43,7 +43,7 @@ class DisqusHelper extends Helper
             if ($this->container->getParameter('kernel.environment') == 'dev') {
                 $error = $e->getMessage();
             } else {
-                $error = 'Ops! Seems there are problem with access to discus.com. Please refresh the page in a few minutes.';
+                $error = 'Oops! Seems there are problem with access to discus.com. Please refresh the page in a few minutes.';
             }
         }
 
