cdb7c7d65d053fdc285772b6a4a211b971e6c47e
Vladimir Sadicov <sadikoff@gmail.com>
2020-05-18 14:29:58 +0300

    remove unused use statement

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,7 +11,6 @@
 
 namespace Knp\Bundle\DisqusBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
